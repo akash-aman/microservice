f1a8e7fca2307c6456d7b89a78a3fe09a99b91db
akash-aman <sir.akashaman@gmail.com>
2025-02-12 23:40:26 +0530

    feat: Enhance GraphQL server with multiple transport options and error handling

--- internal/services/products/app/inits/ef_graphql.go
+++ internal/services/products/app/inits/ef_graphql.go
@@ -1,6 +1,7 @@
 package inits
 
 import (
+	"errors"
 	"net/http"
 	"pkg/logger"
 	"products/cgfx/ent/gen"
@@ -9,24 +10,35 @@ import (
 
 	"entgo.io/contrib/entgql"
 	"github.com/99designs/gqlgen/graphql/handler"
+	"github.com/99designs/gqlgen/graphql/handler/extension"
+	"github.com/99designs/gqlgen/graphql/handler/transport"
 	"github.com/99designs/gqlgen/graphql/playground"
+	"github.com/gorilla/websocket"
 )
 
 func InitGraphQLServer(client *gen.Client, log logger.ILogger) {
-	srv := handler.NewDefaultServer(gql.NewSchema(client))
+	srv := handler.New(gql.NewSchema(client))
 	srv.Use(entgql.Transactioner{TxOpener: client})
 
-	http.Handle("/",
-		playground.Handler("Todo", "/query"),
-	)
+	srv.AddTransport(transport.SSE{})
+	srv.AddTransport(transport.POST{})
+	srv.AddTransport(transport.Websocket{
+		KeepAlivePingInterval: 10 * time.Second,
+		Upgrader: websocket.Upgrader{
+			CheckOrigin: func(r *http.Request) bool {
+				return true
+			},
+		},
+	})
+
+	srv.Use(extension.Introspection{})
+
+	http.Handle("/", playground.Handler("Todo", "/query"))
 	http.Handle("/query", srv)
 
-	log.Infof("GraphQL Server listening on 3001")
-	server := &http.Server{
-		Addr:              ":3001",
-		ReadHeaderTimeout: 30 * time.Second,
-	}
-	if err := server.ListenAndServe(); err != nil {
-		log.Errorf("http server terminated %v", err)
+	log.Infof("Starting GraphQL Server on port :3001")
+
+	if err := http.ListenAndServe(":30001", nil); !errors.Is(err, http.ErrServerClosed) {
+		log.Error("Error starting GraphQL server", err)
 	}
 }
