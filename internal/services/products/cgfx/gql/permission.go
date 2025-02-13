5e9abfdda05bee0383af1c2060c7cd442878e66c
Akash Aman <sir.akashaman@gmail.com>
2025-02-13 11:23:24 +0530

    feat: Implement permission checking in GraphQL resolver

--- internal/services/products/cgfx/gql/permission.go
+++ internal/services/products/cgfx/gql/permission.go
@@ -2,6 +2,7 @@ package gql
 
 import (
 	"context"
+	"errors"
 
 	"github.com/99designs/gqlgen/graphql"
 )
@@ -14,6 +15,17 @@ func HasPermission() func(context.Context, interface{}, graphql.Resolver, []stri
 		permissions []string,
 	) (res interface{}, err error) {
 		// you can do your thing here for permissions
+		// if permissions are not met, return an error
+		if !checkPermissions(ctx, permissions) {
+			return nil, errors.New("unauthorized")
+		}
+
 		return next(ctx)
 	}
+}
+
+// checkPermissions is a placeholder function to check permissions
+func checkPermissions(ctx context.Context, permissions []string) bool {
+	// implement your permission checking logic here
+	return false
 }
